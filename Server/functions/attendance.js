0fb40f61ff9238701815ec0ec215f57396e8e892
Yash Pawar <66915975+yashtheno1@users.noreply.github.com>
2024-05-17 12:01:53 +0000

    fix 3

--- Server/functions/attendance.js
+++ Server/functions/attendance.js
@@ -110,8 +110,105 @@ updateAttendance = (data) => {
     })
 };
 
+fetchalluserAttendance = (data) => {
+    return new Promise(async (resolve, reject) => {
+        dbpool.getConnection((err, conn) => {
+            if (err) {
+                attendanceLogger.trace('customer-attendance-fetchalluserAttendance - ' + data.userId + ' - error in db connection')
+                attendanceLogger.error(err)
+                return reject({ status: 'failed', err: err, data: { bResult: false } });
+            } else {
+                conn.query({
+                    sql: 'SELECT enrollments.*, user_profile.* FROM enrollments JOIN user_profile ON enrollments.User_profile_Id = user_profile.ID JOIN users ON user_profile.userId = users.id WHERE users.id = ?;',
+                    timeout: 40000,
+                    values: [data.userId]
+                }, (error, results) => {
+                    if (error) {
+                        attendanceLogger.trace('customer-attendance-fetchalluserAttendance - ' + data.userId + ' - error in fetching enrollment')
+                        attendanceLogger.error(error)
+                        conn.release();
+                        return reject({ status: 'failed', err: error, data: { bResult: false } });
+                    } else {
+                        conn.release();
+                        return resolve({ status: 'success', msg: 'enrollment fetched', data: { enrollment: results, bResult: true } });
+                    }
+                });
+            }
+        }
+        )
+    }
+    )
+};   
+
+latestattendance = (data) => {
+    return new Promise(async (resolve, reject) => {
+        dbpool.getConnection((err, conn) => {
+            if (err) {
+                attendanceLogger.trace('customer-attendance-fetchalluserAttendance - ' + data.userId + ' - error in db connection')
+                attendanceLogger.error(err)
+                return reject({ status: 'failed', err: err, data: { bResult: false } });
+            } else {
+                conn.query({
+                    sql: 'SELECT attendance.*, user_profile.displayName FROM attendance JOIN enrollments ON attendance.Enrollment_ID = enrollments.ID JOIN user_profile ON enrollments.User_profile_ID = user_profile.ID ORDER BY attendance.ID DESC LIMIT 10;',
+                    timeout: 40000
+                }, (error, results) => {
+                    if (error) {
+                        // var resultsHack = JSON.parse(JSON.stringify(results))
+                        // console.log(results)
+                        attendanceLogger.trace('customer-attendance-fetchalluserAttendance - ' + data.userId + ' - error in fetching attendance')
+                        attendanceLogger.error(error)
+                        conn.release();
+                        return reject({ status: 'failed', err: error, data: { bResult: false } });
+                    } else {
+                        conn.release();
+                        return resolve({ status: 'success', msg: 'attendance fetched', data: { attendance: results, bResult: true } });
+                    }
+                });
+            }
+        }
+        )
+    }
+    )
+}
+
+fetchtotaltime = (data) => {
+    return new Promise(async (resolve, reject) => {
+        dbpool.getConnection((err, conn) => {
+            if (err) {
+                attendanceLogger.trace('customer-attendance-fetchtotaltime - ' + data.userId + ' - error in db connection')
+                attendanceLogger.error(err)
+                return reject({ status: 'failed', err: err, data: { bResult: false } });
+            } else {
+                conn.query({
+                    sql: 'SELECT SUM(session) as totalTime FROM `attendance` WHERE `Enrollment_ID` = ?;',
+                    timeout: 40000,
+                    values: [data.enrollmentId]
+                }, (error, results) => {
+                    if (error) {
+                        attendanceLogger.trace('customer-attendance-fetchtotaltime - ' + data.userId + ' - error in fetching attendance')
+                        attendanceLogger.error(error)
+                        conn.release();
+                        return reject({ status: 'failed', err: error, data: { bResult: false } });
+                    }
+                    else {
+                        resultsHack = JSON.parse(JSON.stringify(results))
+                        let totalTime = 0;
+                        resultsHack.forEach((data) => {
+                            totalTime += data.session;
+                        });
+                        conn.release();
+                        return resolve({ status: 'success', msg: 'attendance fetched', data: { totalTime: resultsHack[0].totalTime, bResult: true } });
+                    }
+                })
+            }
+        })
+    })
+};
 module.exports = {
     fetchAttendance,
     addAttendance,
-    updateAttendance
+    updateAttendance,
+    fetchalluserAttendance,
+    latestattendance,
+    fetchtotaltime
 };
